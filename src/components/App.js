9e5981e8995fc26f626577e44818686a072108e6
hls93 <hlshaw3@ncsu.edu>
2017-12-29 12:15:48 -0500

    organizing files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="tile">
+          <div className="my-header">EarthQuakes!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
